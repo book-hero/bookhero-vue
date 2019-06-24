a5f47191644be81e1a0216c7600599fd9d16b16c
Logan Waite <lwaite2010@gmail.com>
2019-06-23 21:04:24 -0600

    update api for production endpoint

--- src/api.js
+++ src/api.js
@@ -1,55 +1,47 @@
 import axios from 'axios'
 const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/' : ''
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : 'http://api.bookhero.net/'
 
 function isFunction(functionToCheck) {
-  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]'
+  return functionToCheck && typeof (functionToCheck) === 'function'
 }
 
-async function makeCall(apiCall, params) {
-  if (!isFunction(apiCall)) throw Error('apiCall is not a function')
+async function makeCall(axiosMethod, params) {
+  if (!isFunction(axiosMethod)) throw Error('axiosMethod is not a function')
   console.log(...params)
-  try {
-    const response = await apiCall(...params)
-    return response.data
-  } catch (e) {
-    console.error({ errors: e.response.data })
-    return { errors: e.response.data }
-  }
+  return axiosMethod(...params)
+    .then(response => response.data)
+    .catch(error => ({ errors: error.response.data }))
 }
 
-function calls(base) {
+function apiFactory(base) {
   return {
-    get(additionalUrl, params) {
-      const url = base + (additionalUrl || '') + '/'
+    get(additionalUrl = '', params = {}) {
+      const url = `${base}${additionalUrl}/`
       return makeCall(axios.get, [url, { params }])
     },
-    post(additionalUrl, body) {
-      const url = base + (additionalUrl || '') + '/'
+    post(additionalUrl = '', body = {}) {
+      const url = `${base}${additionalUrl}/`
       return makeCall(axios.post, [url, body])
     },
     put(url) {
       axios.put(base + url || '')
     },
-    delete(additionalUrl, params) {
-      const url = base + (additionalUrl || '') + '/'
+    delete(additionalUrl = '', params = {}) {
+      const url = `${base}${additionalUrl}/`
       return makeCall(axios.delete, [url, { params }])
     },
-    patch(additionalUrl, body) {
-      const url = base + (additionalUrl || '') + '/'
+    patch(additionalUrl = '', body = {}) {
+      const url = `${base}${additionalUrl}/`
       return makeCall(axios.patch, [url, body])
     }
   }
 }
 
-export const booksApi = (() => {
-  return calls(BASE_URL + 'books')
-})()
+export const booksApi = (() => apiFactory(BASE_URL + 'books'))()
 
-export const authApi = (() => {
-  return calls(BASE_URL + 'auth')
-})()
+export const authApi = (() => apiFactory(BASE_URL + 'auth'))()
 
-export const userApi = (() => {
-  return calls(BASE_URL + 'user')
-})()
+export const userApi = (() => apiFactory(BASE_URL + 'user'))()
+
+export const attributesApi = (() => apiFactory(BASE_URL + 'attributes'))()
